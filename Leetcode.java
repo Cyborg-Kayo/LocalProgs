2bf10c5d6ac460dda06e13b9de721e0c27e456e1
Vishnu <vishnurajj03@gmail.com>
2024-04-07 17:37:17 +0530

    Updated Prog Without Py

--- Leetcode.java
+++ Leetcode.java
@@ -3,6 +3,18 @@ import java.util.*;
 
 public class Leetcode {
     public static void main(String[] args) {
+        HashSet<Character> x=new HashSet<>();
+        String s="Vish n    u";
+        String[] v=s.split(" +");
+        for(String i:v){
+            System.out.println(i);
+        }
+        // x.add('a');
+        // x.add('b');
+        // x.add('c');
+        // x.add('d');
+        // System.out.println(x.size());
+        
         
     }
 }
