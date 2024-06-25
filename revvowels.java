ddbe07503d12d79da04b7adbb869eba5946751ff
Vishnu <vishnurajj03@gmail.com>
2024-06-25 18:19:41 +0530

    adding revstrings

---
+++ revvowels.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class revvowels {
+    public static void main(String[] args) {
+        Character[] vowel={'a','e','i','o','u','A','E','I','O','U'};
+        for(Character c:vowel){
+            System.out.println(c);
+        }
+
+        // List<Character> vowels=new ArrayList<>(Arrays.asList(vowel));
+        // System.out.println(vowels);
+    }
+}
